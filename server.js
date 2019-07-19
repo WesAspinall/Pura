dd107264274ee42660aaac27a53902e9299577af
wesaspinall <wes@wesaspinall.com>
2019-07-18 22:37:27 -0400

    basic front end

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 8080;
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => {
+    res.send('hello')
+});
 
 
-
-
+app.listen(port, () => console.log(`your app is running on port ${port}`));
